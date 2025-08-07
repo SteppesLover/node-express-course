af842d142c940c056d9c477cd0511ab561471f1a
SteppesLover <amanzhanovalikhan@gmail.com>
2025-08-07 08:13:49 -0700

    Lesson-01-assignment

--- 01-node-tutorial/answers/08-os-module.js
+++ 01-node-tutorial/answers/08-os-module.js
@@ -1,10 +1,8 @@
 const os = require('os')
 
-// info about current user
 const user = os.userInfo()
 console.log(user)
 
-//mothod returns the system uptime in seconds
 console.log(`The System Uptime is ${os.uptime()} seconds`)
 
 const currentOs = {
---
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -0,0 +1,29 @@
+const { error } = require('console');
+const {readFile, writeFile} = require('fs');
+
+console.log('start');
+readFile('./01-node-tutorial/answers/content/first.txt', 'utf8', (err,result) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./01-node-tutorial/answers/content/second.txt', 'utf8', (err,result) => {
+        if(err){
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile(
+            './01-node-tutorial/answers/content/result-async.txt', 
+            `Here is the result: ${first}, ${second}`
+        ,(err, result) =>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            console.log('done with this task');
+        })
+    })
+})
+console.log('starting next task')
