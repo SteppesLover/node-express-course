7afd0a43bbee2a9de3f847e01638f21aa0f5879f
SteppesLover <amanzhanovalikhan@gmail.com>
2025-11-11 18:00:39 -0800

    added cookie for Lesson-04-assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express ()
 const peopleRouter = require('./routes/people')
-const auth = require('./routes/auth')
 const logger = require('./logger')
+const cookieParser = require('cookie-parser')
 app.get('/', logger, (req, res) => {
     
 })
@@ -15,14 +15,40 @@ app.use(express.urlencoded({extended: false}))
 
 //parse json
 app.use(express.json())
+app.use(cookieParser())
 app.use(logger)
 
 app.use('/api/people', peopleRouter)
-app.use('/login', auth)
+
+const auth = (req, res, next) => {
+  const { name } = req.cookies
+  if (name) {
+    req.user = name
+    next()
+  } else {
+    res.status(401).json({ success: false, msg: 'Unauthorized' })
+  }
+}
+
+app.post('/logon', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'Please provide a name' })
+  }
+  res.cookie('name', name, { httpOnly: true })
+  res.status(201).json({ success: true, msg: `Hello, ${name}! You are logged in.` })
+})
+
+app.delete('/logoff', (req, res) => {
+  res.clearCookie('name')
+  res.status(200).json({ success: true, msg: 'You are logged off.' })
+})
+
+app.get('/test', auth, (req, res) => {
+  res.status(200).json({ success: true, msg: `Welcome back, ${req.user}!` })
+})
 
 
-
-
-app.listen(5000, () => {
-    console.log('server is listening on port 5000....')
+app.listen(3000, () => {
+    console.log('server is listening on port 3000....')
 })
