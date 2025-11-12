1e0fe510a4ecb7e06de48bd6580dedcf1f10099e
SteppesLover <amanzhanovalikhan@gmail.com>
2025-11-11 17:41:00 -0800

    Lesson-04-assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,51 +1,28 @@
 const express = require('express');
-const app = express();
-const { products } = require('./data');
+const app = express ()
+const peopleRouter = require('./routes/people')
+const auth = require('./routes/auth')
+const logger = require('./logger')
+app.get('/', logger, (req, res) => {
+    
+})
 
-app.use(express.static('./public'));
+//static assets
+app.use(express.static('./methods-public'))
+//parse form data
 
-app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
-app.get('/api/v1/products', (req, res) => {
-  res.json(products);
-});
+//parse json
+app.use(express.json())
+app.use(logger)
 
-app.get('/api/v1/products/:productID', (req, res) => {
-  const idToFind = parseInt(req.params.productID);
-  const product = products.find((p) => p.id === idToFind);
-  if (!product) {
-    return res.status(404).json({ message: 'That product was not found.' });
-  }
-  res.json(product);
-});
+app.use('/api/people', peopleRouter)
+app.use('/login', auth)
 
-app.get('/api/v1/query', (req, res) => {
-  const { search, limit, maxPrice } = req.query;
-  let filtered = [...products];
 
-  if (search) {
-    filtered = filtered.filter((p) =>
-      p.name.toLowerCase().startsWith(search.toLowerCase())
-    );
-  }
 
-  if (maxPrice) {
-    const priceLimit = parseFloat(maxPrice);
-    filtered = filtered.filter((p) => p.price <= priceLimit);
-  }
 
-  if (limit) {
-    filtered = filtered.slice(0, Number(limit));
-  }
-
-  if (filtered.length < 1) {
-    return res.status(200).json({ message: 'No products matched your criteria.' });
-  }
-
-  res.json(filtered);
-});
-
-const port = 5000;
-app.listen(port, () => {
-  console.log(` Server running at http://localhost:${port}`);
-});
+app.listen(5000, () => {
+    console.log('server is listening on port 5000....')
+})
